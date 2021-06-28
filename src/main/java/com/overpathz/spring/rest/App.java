0352889e542ce5a68c3d16bd87bbd47a85bb223b
overpathz <overpathz@gmail.com>
2021-06-29 01:36:51 +0300

    first commit

---
+++ src/main/java/com/overpathz/spring/rest/App.java
@@ -0,0 +1,13 @@
+package com.overpathz.spring.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
