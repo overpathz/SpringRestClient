bb67635b92f7305a9cb00610f0d9b80643f09ca7
overpathz <overpathz@gmail.com>
2021-06-29 12:49:04 +0300

    final version

--- src/main/java/com/overpathz/spring/rest/App.java
+++ src/main/java/com/overpathz/spring/rest/App.java
@@ -1,9 +1,7 @@
 package com.overpathz.spring.rest;
 
 import com.overpathz.spring.rest.configuration.MyConfig;
-import com.overpathz.spring.rest.entity.BankCurrency;
 import com.overpathz.spring.rest.entity.Employee;
-import com.overpathz.spring.rest.entity.PersonalData;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -12,8 +10,11 @@ import java.util.List;
 
 public class App {
     public static void main( String[] args ) {
+
         ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
         Communication communication = context.getBean("communication", Communication.class);
-        communication.deleteEmployee(6);
+
+        Employee getEmployee = communication.getEmployee(4);
+        System.out.println(getEmployee);
     }
 }
